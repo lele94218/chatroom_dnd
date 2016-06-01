e6e9c19e9882d83e1c74c46d244af18dc942ca69
xuetaoran <914174064@qq.com>
2016-06-01 15:13:37 +0800

    添加 sample

---
+++ src/main/java/com/terryx/chatroom/sample/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.terryx.chatroom.sample;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author xueta on 2016/6/1 15:06.
+ */
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
